9dd22c16dcaa1c2416929d0f47298cb923b673f0
MateusHerbele <mateusherbelemc@gmail.com>
2024-07-29 11:53:47 -0300

    [organização] organização dos arquivos

--- node.py
+++ node.py
@@ -1,13 +1,44 @@
 import socket
+from game import Game
+from player import Player
+from network import send_broadcast, send_unicast, ring_messages
+from utils import *
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!".encode('utf-8')
+# --------main--------
+def main():
+    CURRENT_NODE_ADDRESS, NEXT_NODE_ADDRESS = get_addresses()
 
-print(f"UDP target IP: {UDP_IP}")
-print(f"UDP target port: {UDP_PORT}")
-print(f"message: {MESSAGE}")
+    socket_sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_receiver.bind(CURRENT_NODE_ADDRESS[0])
 
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    token_available = True if CURRENT_NODE_ADDRESS[1] == 0 else False
+    start_game = True if CURRENT_NODE_ADDRESS[1] == 0 else False
+    player = Player()
+    game = Game()
+    game_state = 0
+    wait_for_user_input()
+    while True:
+        # Inicializar o jogo
+        if start_game:
+            game_state = player.dealer_routine(CURRENT_NODE_ADDRESS[1], game, socket_sender, socket_receiver, NEXT_NODE_ADDRESS)
+            token_available = False
+            start_game = False
+        # Verificar se o token está disponível
+        if token_available:
+            game_state = player.dealer_routine(CURRENT_NODE_ADDRESS[1], game, socket_sender, socket_receiver, NEXT_NODE_ADDRESS)
+            token_available = False  # Token usado
+        # Se não tiver, só espera as mensagens
+        elif game_state == 0: # Continua o jogo
+            # Receber as mensagens
+            # Essa função só para quando receber o token
+            network_comunication = ring_messages(socket_sender, socket_receiver)
+            if network_comunication == 1:
+                token_available = True
+            if network_comunication == 0:
+                break # Acaba a execução do programa
+        else: # Fim do jogo
+            break
+if __name__ == "__main__":
+    main()
